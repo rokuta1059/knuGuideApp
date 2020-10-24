e9caa8433c01b8af1fb98b3a7852737bf84eb225
KJY <rave189@naver.com>
2020-10-24 21:16:31 +0900

    RecyclerViewAdapter 추가
    add code

--- app/KnuGuide/app/src/main/java/com/knu/knuguide/view/adapter/RecyclerViewAdapter.kt
+++ app/KnuGuide/app/src/main/java/com/knu/knuguide/view/adapter/RecyclerViewAdapter.kt
@@ -1,10 +1,41 @@
 package com.knu.knuguide.view.adapter
 
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.knu.knuguide.R
+import com.knu.knuguide.data.KNUData
+import com.knu.knuguide.data.cafeteria.Cafeteria
+import kotlinx.android.synthetic.main.item_cafeteria.view.*
 
-class RecyclerViewAdapter{
-    class myViewHolder(){
+class RecyclerViewAdapter(
+    private val context: Context,
+    private var items: ArrayList<KNUData>) : RecyclerView.Adapter<RecyclerView.ViewHolder>(){
 
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        val inflater = LayoutInflater.from(context)
+        val view = inflater.inflate(R.layout.item_cafeteria, parent, false)
+        return CafeteriaViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        val item = items[position]
+        bindCafeteria(item as Cafeteria, holder as CafeteriaViewHolder)
+    }
+
+    class CafeteriaViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView)
+
+    private fun bindCafeteria(cafeteria: Cafeteria, holder: CafeteriaViewHolder){
+        holder.itemView.todayFood.text = cafeteria.lunch
+    }
+
+    override fun getItemViewType(position: Int): Int {
+        return items[position].getRecyclerType()
+    }
+
+    override fun getItemCount(): Int {
+        return items.size
     }
 }
